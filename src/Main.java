c1f7b0055003f4347334865c9836f487f1af4211
yeseoung <kys030925@naver.com>
2023-01-08 03:16:03 +0900

    duplicate

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import com.sun.jdi.Method;
 
+import java.util.Arrays;
+
+
 public class Main {
-    public static int[] test_set = {1,2,3,4,5,6,7,8,9,10,11};//input set
+    public static int[] test_set = {0,1,2,3,4,5};//input set
     public static void main(String[] args) {
-        Arrays arrays = new Arrays();
-        arrays.print(test_set);
-        arrays.outIndex(test_set,9);
-        System.out.println("");
-        arrays.print(test_set);
+        Array arrays = new Array();
+
+
 
 
 
